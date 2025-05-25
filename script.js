7f8c572a7d557da2fe9a53ff23f44d206612489f
Muhammad Mubarak <muhammadmarch09@gmail.com>
2025-05-25 22:29:52 +0600

    creating the playround function done

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// Function for computer choice
+
 function getComputerChoice() {
   let randomNumber = Math.floor(Math.random() * 3);
   if (randomNumber === 0) {
@@ -8,5 +10,44 @@ function getComputerChoice() {
     return "scissors";
   }
 }
+// Function for Human Choice
 
-console.log(getComputerChoice());
+function getHumanChoice() {
+  let humanInput = prompt().toLowerCase();
+  return humanInput;
+}
+
+let humanChoice = getHumanChoice();
+let computerChoice = getComputerChoice();
+
+// Score Variables
+
+// PlayRound Function
+function playRound(humanChoice, computerChoice) {
+  let humanScore = 0;
+  let computerScore = 0;
+  if (humanChoice === computerChoice) {
+    console.log("Tie!");
+  } else if (humanChoice === "rock" && computerChoice === "scissors") {
+    return "Rock beats scissors. You win!";
+    humanScore++;
+  } else if (humanChoice === "paper" && computerChoice === "rock") {
+    return "Paper beats rock. You win!";
+    humanScore++;
+  } else if (humanChoice === "scissors" && computerChoice === "paper") {
+    return "Scissors beats paper. You win!";
+    humanScore++;
+  } else if (humanChoice === "rock" && computerChoice === "paper") {
+    return "Paper beats rock. You lose!";
+    computerScore++;
+  } else if (humanChoice === "paper" && computerChoice === "scissors") {
+    return "Scissors beats paper. You lose!";
+    computerScore++;
+  } else if (humanChoice === "scissors" && computerChoice === "rock") {
+    return "Rock beats scissors. You lose!";
+    computerScore++;
+  }
+  
+}
+console.log(playRound(humanChoice, computerChoice));
+
